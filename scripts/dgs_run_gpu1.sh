0b092be4f187289b80bf3d113de8c19d5d03f0a0
iMvijay23 <vijumuraari@gmail.com>
2024-06-26 10:08:58 -0400

    script for ica100 and empathy prompt

--- scripts/dgs_run_gpu1.sh
+++ scripts/dgs_run_gpu1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=dgs_run1
+#SBATCH --job-name=dgs_run3
 #SBATCH --time=48:00:00
-#SBATCH --partition=a100
+#SBATCH --partition=ica100
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
 #SBATCH --qos=qos_gpu
 #SBATCH --mail-user=vtiyyal1@jh.edu
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type BEGIN
+#SBATCH --mail-type END
+#SBATCH --mail-type FAIL
 #SBATCH -A mdredze80_gpu
-#SBATCH --output="/home/vtiyyal1/askdocs/outputs/dgs_run_2.out"
+#SBATCH --output="/home/vtiyyal1/askdocs/outputs/dgs_run_3.out"
 #SBATCH --export=ALL
 
 module load anaconda
@@ -21,9 +21,8 @@ module load cuda/11.7
 conda info --envs
 
 conda activate llmtrain_env
-pip install --upgrade openai
-
+#pip install openai
 export TOKENIZERS_PARALLELISM=false
 echo "Running python script..."
 
-python dgs.py --use_quantize 0 --use_openai_for_facts False
+python dgsica.py --use_quantize 0 --use_openai_for_facts False
